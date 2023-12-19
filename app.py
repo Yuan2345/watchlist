8e16493afc00e8ff69ac08813fa0ca6cf9f67c53
YJY <1415723955@qq.com>
2023-12-19 14:40:18 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -1,28 +1,20 @@
-from flask import Flask, render_template
-
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    user = User.query.first()  # 读取用户记录
-    movies = Movie.query.all()  # 读取所有电影记录
-    return render_template('index.html', user=user, movies=movies)
-
-
-
 import os
 import sys
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
 # 在扩展类实例化前加载配置
 db = SQLAlchemy(app)
+
+
+@app.route('/')
+def index():
+    movies = Movie.query.all()  # 读取所有电影记录
+    return render_template('index.html',  movies=movies)
 
 class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
     id = db.Column(db.Integer, primary_key=True)  # 主键
@@ -64,6 +56,16 @@ def forge():
 
     db.session.commit()
     click.echo('Done.')
+
+@app.context_processor
+def inject_user():  # 函数名可以随意修改
+    user = User.query.first()
+    return dict(user=user)  # 需要返回字典，等同于 return {'user': user}
+
+
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):  # 接受异常对象作为参数
+    return render_template('404.html'), 404  # 返回模板和状态码
 
 
 
